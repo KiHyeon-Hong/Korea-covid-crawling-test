343975a1e7307ec406c1a4c31b0c9325b0bfb710
KiHyeon-Hong <ghdrlgus96@gmail.com>
2020-12-01 23:30:43 +0900

    add covidCrawlingServer
    add covidCrawlingServer

--- controller/index.js
+++ controller/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const fs = require('fs');
+const mysql = require('mysql');
 var exec = require('child_process').exec;
 
 const router = express.Router();
 
+const client = mysql.createConnection({
+  host: 'localhost', // DB서버 IP주소
+  port: 3306, // DB서버 Port주소
+  user: 'root', // DB접속 아이디
+  password: 'gachon654321', // DB암호
+  database: 'covid' //사용할 DB명
+});
+
 router.get('/', (request, response) => {
-  exec('python3 module/covidCrawling.py', function callback(error, stdout, stderr){
-    response.send('sucess');
+  client.query('SELECT * FROM CovidInfo order by day desc limit 1', (error, results) => {
+    response.send(results);
   });
 });
 
@@ -16,6 +25,7 @@ router.get('/patientNow', (request, response) => {
   data = JSON.parse(data);
 
   response.send((data.patientNow).toString());
+
 });
 
 router.get('/examinationNow', (request, response) => {

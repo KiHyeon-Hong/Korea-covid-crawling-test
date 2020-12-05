ed3691a8ed241347286063ad8bf28f262520122b
KiHyeon-Hong <ubuntu@ip-172-31-92-56.ec2.internal>
2020-12-05 08:55:00 +0000

    IP add

--- mainController.js
+++ mainController.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const ejs = require('ejs');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const request = require('request');
 
 const app = express();
 
@@ -18,5 +19,11 @@ const index = require('./controller/index.js');
 app.use('/', index);
 
 app.listen(65001, () => {
-	console.log('server running at 65001');
+	request.get(
+		{url:'https://api.ipify.org'},
+		function(err, response, body) {
+				fs.writeFileSync(__dirname + '/files/webPage.txt', body + ':65001', 'utf8')
+				console.log('server running at ' + body + ':65001');
+		}
+	);
 });
